e6209639600a74b261c22dd8d480858ee1f44fc3
ayethandaraung-coder <ayethandaraung@mabbank.com>
2021-06-09 09:30:10 +0630

    Update for system close tab event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {NavigationEnd, Router} from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { BnNgIdleService } from 'bn-ng-idle';
 import { User } from 'src/models/User';
 import { AuthenticationService } from 'src/services/AuthenticationService';
+import { environment } from "src/environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, Observable } from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -11,18 +14,35 @@ import { AuthenticationService } from 'src/services/AuthenticationService';
 })
 export class AppComponent implements OnInit {
   title = 'app';
-  currentUser : User;
-  constructor(private authService:AuthenticationService,private bnIdle: BnNgIdleService,private router: Router) {
-   }
+  currentUser: User;
+  private currentUserSubject: BehaviorSubject<User>;
+  constructor(private authService: AuthenticationService, private bnIdle: BnNgIdleService, private router: Router,
+    private http: HttpClient) {
+  }
 
   ngOnInit() {
+
     this.router.events.subscribe((evt) => {
       if (!(evt instanceof NavigationEnd)) {
         return;
       }
       window.scrollTo(0, 0);
     });
+
     this.authService.currentUser.subscribe(x => this.currentUser = x);
+    let context = this;
+    window.addEventListener("beforeunload", function (e) {
+      e = e || window.event;
+      if (e.isTrusted) {
+        //e.returnValue = 'Sure?';
+        let currentUser: User = JSON.parse(localStorage.getItem('currentUser'));
+        console.log("currentUser" + currentUser);
+        if (currentUser) {
+          context.authService.applicationBackend_logout(currentUser.userId);
+        }
+      }
+    });
+
     /*this.bnIdle.startWatching(30).subscribe((isTimedOut: boolean) => {
       if (isTimedOut) {
         console.log('session expired');
@@ -31,6 +51,5 @@ export class AppComponent implements OnInit {
       }
     });*/
   }
-  
 
 }
--- src/services/AuthenticationService.ts
+++ src/services/AuthenticationService.ts
@@ -8,23 +8,27 @@ import { User } from "src/models/User";
 
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
+
     private currentUserSubject: BehaviorSubject<User>;
     public currentUser: Observable<User>;
-    constructor(private http: HttpClient,private router: Router) {
+    constructor(private http: HttpClient, private router: Router) {
         this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
         this.currentUser = this.currentUserSubject.asObservable();
     }
+
     public get currentUserValue(): User {
         return this.currentUserSubject.value;
     }
+
     hasRole(role: string) {
-        var user :User;
-        return  this.currentUser.subscribe(x => user = x);
+        var user: User;
+        return this.currentUser.subscribe(x => user = x);
         return user.role.indexOf(role) === -1;
     }
+
     login(userId: string, password: string) {
         var username = userId;
-        return this.http.post<any>(`${environment.baseUrl}/authenticate`, { userId,username, password })
+        return this.http.post<any>(`${environment.baseUrl}/authenticate`, { userId, username, password })
             .pipe(map(user => {
                 // store user details and jwt token in local storage to keep user logged in between page refreshes
                 localStorage.setItem('currentUser', JSON.stringify(user));
@@ -36,14 +40,18 @@ export class AuthenticationService {
     logout() {
         localStorage.removeItem('currentUser');
         this.currentUserSubject.next(null);
-          }
-    applicationBackend_logout(userId: string){
-        var url = "/authenticate_logout?userId="+userId;
-        this.http.get(`${environment.baseUrl}`+url,{responseType: 'text'}).subscribe(res =>{
+    }
+
+    applicationBackend_logout(userId: string) {
+        var url = "/authenticate_logout?userId=" + userId;
+        this.http.get(`${environment.baseUrl}` + url, { responseType: 'text' }).subscribe(res => {
             // remove user from local storage to log user out
-             localStorage.removeItem('currentUser');
-             this.currentUserSubject.next(null);
-             this.router.navigate(['/auth/login']);
-         });
-          }
+            // localStorage.removeItem('currentUser');
+            // this.currentUserSubject.next(null);
+            // this.router.navigate(['/auth/login']);
+        });
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+        this.router.navigate(['/auth/login']);
+    }
 }
