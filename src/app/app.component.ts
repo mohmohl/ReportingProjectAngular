64fde0e02036ad827a8e3b55918db35ff478518d
ayethandaraung-coder <ayethandaraung@mabbank.com>
2021-06-14 09:24:00 +0630

    update loan css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,7 +43,6 @@ export class AppComponent implements OnInit {
     });
 
     this.bnIdle.startWatching(3600).subscribe((isTimedOut: boolean) => {
-
       if (isTimedOut) {
         console.log('Application session expired');
         this.authService.applicationBackend_logout(this.authService.currentUserValue.userId);
