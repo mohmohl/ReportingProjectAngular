a3e1bc2c1f4d22822b56f96152e558f6036925d8
DESKTOP-4VVSL1A\bankuser <marmarmyint@mabbank.com>
2022-06-22 13:31:34 +0630

    obr encash ccsi

---
+++ src/app/views/obrencashschccsi/obrencashschccsi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-obrencashschccsi',
+  templateUrl: './obrencashschccsi.component.html',
+  styleUrls: ['./obrencashschccsi.component.css']
+})
+export class ObrencashschccsiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
