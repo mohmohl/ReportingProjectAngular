3ee6bd83e3e17c2387d414e9088d2c7294eafd8a
KhantKoKo-Mab <82010729+KhantKoKo-Mab@users.noreply.github.com>
2022-08-02 13:59:52 +0630

    deploy uat

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 
 export const environment = {
   production: true,
-   baseUrl : 'http://172.30.10.90:7503/reporting_frontend_service_UAT_V2'
-   
-   //'http://172.30.10.90:7503/reporting_frontend_service'
+  baseUrl: 'http://172.30.10.90:7503/reporting_frontend_service'
+
+  //'http://172.30.10.90:7503/reporting_frontend_service'
   //baseUrl : 'http://172.30.4.51:7003/reporting_frontend_service'
   //baseUrl : 'http://172.30.10.90:7503/reporting_frontend_service_bypass'
   // baseUrl : 'http://172.30.10.90:7503/reporting_frontend_service_V12_MOCK'
