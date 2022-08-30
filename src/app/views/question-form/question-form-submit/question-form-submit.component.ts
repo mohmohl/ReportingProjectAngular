e523680dfe8a7525898942575ec9cb16ff2eaa59
YeYintAung02MAB <yeyintaung02@mabbank.com>
2022-08-30 15:58:40 +0630

    commit limit answer

--- src/app/views/question-form/question-form-submit/question-form-submit.component.ts
+++ src/app/views/question-form/question-form-submit/question-form-submit.component.ts
@@ -5,6 +5,8 @@ import { Option } from 'src/models/question_form/Option';
 import { Question } from 'src/models/question_form/Question';
 import { TopicDetail } from 'src/models/question_form/TopicDetail';
 import { User } from 'src/models/User';
+import { QNAAnswerLogService } from 'src/services/qnaanswer-log.service';
+import { AnswerLog } from 'src/models/question_form/AnswerLog';
 
 @Component({
   selector: 'app-question-form-submit',
@@ -13,18 +15,42 @@ import { User } from 'src/models/User';
 })
 export class QuestionFormSubmitComponent implements OnInit {
 
+  topic_id: string;
   topic: TopicDetail;
   remaningQuestions: Question[] = [];
   remainWarningMsg = 'No answer is choosen';
 
-  constructor(private router: Router, private activeRoute: ActivatedRoute, private questionFormServie: QuestionFormService) { }
+  constructor(private router: Router, private activeRoute: ActivatedRoute, private qNAAnswerLogService: QNAAnswerLogService, private questionFormServie: QuestionFormService) { }
 
   ngOnInit() {
+
+
+
     this.activeRoute.params.subscribe((params) => {
       if (params && params['param1']) {
-        this.questionFormServie.getQuestionFormByID(params['param1']).subscribe((res) => {
-          this.topic = res;
-          console.log(JSON.stringify(res));
+        let topicId = params['param1'];
+        this.qNAAnswerLogService.getCandidateAnswerLog(topicId).subscribe((answerLog: AnswerLog) => {
+
+          if (answerLog != null && answerLog.try_count == 3) {
+            this.router.navigate(['/question-form-list']);
+          }
+
+          this.questionFormServie.getQuestionFormByID(topicId).subscribe((res) => {
+
+            this.qNAAnswerLogService.updateCandidateAnswerLog(topicId).subscribe((updateRes) => {
+
+              this.topic = res;
+              this.topic_id = this.topic.id;
+
+            }, error => {
+              this.router.navigate(['/question-form-list']);
+            }
+            );
+
+          }, error => {
+            this.router.navigate(['/question-form-list']);
+          });
+
         });
       }
     });
@@ -47,31 +73,42 @@ export class QuestionFormSubmitComponent implements OnInit {
   }
 
   onSubmit() {
-    this.remaningQuestions = this.topic.questions.filter(q => q.options.every(op => op.is_chosen == 0) == true);
 
-    // this.remaningQuestions = this.topic.questions.filter(q => {
-    //   let total = q.options.reduce((sum, current) => {
-    //     if (current.user_chosen === '0')
-    //       return sum;
-    //     else
-    //       return ++sum;
-    //   }, 0);
-    //   return total == 0;
-    // }) 
+    this.qNAAnswerLogService.getCandidateAnswerLog(this.topic.id).subscribe((answerLog: AnswerLog) => {
 
-    let login_user: User = JSON.parse(localStorage.getItem('currentUser'));
-    this.topic.user_id = login_user.userId;
+      if (answerLog != null && answerLog.try_count == 3) {
+        this.router.navigate(['/question-form-list']);
+      } else {
+        this.remaningQuestions = this.topic.questions.filter(q => q.options.every(op => op.is_chosen == 0) == true);
 
-    if (this.remaningQuestions && this.remaningQuestions.length > 0) {
-      if (confirm("You have not choosen one or more questions. Do you want to submit?") == true) {
-        console.log(JSON.stringify(this.topic));
-        this.questionFormServie.submitQuestion(this.topic).subscribe((res) => { });
+        // this.remaningQuestions = this.topic.questions.filter(q => {
+        //   let total = q.options.reduce((sum, current) => {
+        //     if (current.user_chosen === '0')
+        //       return sum;
+        //     else
+        //       return ++sum;
+        //   }, 0);
+        //   return total == 0;
+        // }) 
+
+        let login_user: User = JSON.parse(localStorage.getItem('currentUser'));
+        this.topic.user_id = login_user.userId;
+
+        if (this.remaningQuestions && this.remaningQuestions.length > 0) {
+          if (confirm("You have not choosen one or more questions. Do you want to submit?") == true) {
+            console.log(JSON.stringify(this.topic));
+            this.questionFormServie.submitQuestion(this.topic).subscribe((res) => { });
+          }
+        }
+        else {
+          this.questionFormServie.submitQuestion(this.topic).subscribe((res) => { });
+          console.log(JSON.stringify(this.topic));
+        }
       }
-    }
-    else {
-      this.questionFormServie.submitQuestion(this.topic).subscribe((res) => { });
-      console.log(JSON.stringify(this.topic));
-    }
+
+    });
+
+
   }
 
   onBack() {
---
+++ src/models/question_form/AnswerLog.ts
@@ -0,0 +1,5 @@
+export class AnswerLog{
+    user_id: String;
+    topic_id: String;
+    try_count: Number;
+}
