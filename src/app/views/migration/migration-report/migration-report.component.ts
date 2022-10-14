e0fb0dd1f2f41c2c6ebc3537fbba045ffc694304
mohmoh <82033917+mohmohl@users.noreply.github.com>
2022-10-14 22:10:53 +0630

    commit

--- src/app/views/migration/migration-report/migration-report.component.ts
+++ src/app/views/migration/migration-report/migration-report.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { map } from 'rxjs/operators';
+import { CommonModel } from 'src/models/CommonModel';
 import { BackDateReportService } from 'src/services/BackDateReportService';
 
 @Component({
@@ -11,13 +12,13 @@ import { BackDateReportService } from 'src/services/BackDateReportService';
 export class MigrationReportComponent implements OnInit {
 
   branch_list:string[];
-  report_list:string[]=[];
+  report_list:CommonModel[]=[];
+  file:CommonModel=new CommonModel();
   loading = false;
   branch: string;
   fileName: string;
   version:string;
   error = '';
-
   form = new FormGroup({
     branch: new FormControl('',Validators.required),
     version:new FormControl('V12',Validators.required),
@@ -33,6 +34,7 @@ export class MigrationReportComponent implements OnInit {
     this.loading = true
     this.bdService.getBranchListForMigration().subscribe(res =>{
       this.loading = false
+    
       this.branch_list=res;
     },error=>{
       this.loading = false
@@ -40,18 +42,25 @@ export class MigrationReportComponent implements OnInit {
     });
 
   }
-
+  onSelected(value:string): void {
+		alert(value);
+	}
   submit() {
     if (this.form.invalid) {
       this.error = "Data is required";
       return;
     }
-    this.loading = true;
+   
     this.error = "";
     this.branch = this.form.get(["branch"])!.value;
     this.fileName=this.form.get(["report_name"])!.value;
+    let file_name= this.fileName;
     this.version=this.form.get(["version"])!.value;
+    let nameObj:CommonModel[] =  this.report_list.filter(function(e) {
+      return e.report_name == file_name;
+    });
     
+    this.loading = true;
     this.bdService.exportMigration_report(this.fileName,this.branch,this.version).pipe(
       map((data: any) => {
         let blob = new Blob([data], {
@@ -60,7 +69,7 @@ export class MigrationReportComponent implements OnInit {
 
         var link = document.createElement('a');
         link.href = window.URL.createObjectURL(blob);
-        link.download = this.fileName+'.csv';
+        link.download = nameObj[0].report_no+"-"+nameObj[0].report_name+"_"+this.version+"_"+this.branch+'.csv';
         link.click();
         window.URL.revokeObjectURL(link.href);
 
@@ -72,14 +81,13 @@ export class MigrationReportComponent implements OnInit {
             this.loading = false;
           });
 
-
-
   }
 set_report_name(){
   this.loading = true
-    this.bdService.getReportListForMigration().subscribe(res =>{
+    this.bdService.getReportListForMigration().subscribe((res:CommonModel[]) =>{
       this.loading = false
       this.report_list=res;
+
     },error=>{
       this.loading = false
       this.error="System have the error!..."
