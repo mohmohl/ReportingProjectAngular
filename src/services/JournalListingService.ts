c30c69e70a74590aaa4181e11f4547c78caa9dac
WaiHninLwin <82025765+WaiHninLwin@users.noreply.github.com>
2022-11-24 22:36:07 +0630

    change design in Journal Listing Report

--- src/services/JournalListingService.ts
+++ src/services/JournalListingService.ts
@@ -2,6 +2,7 @@ import { Injectable, Optional } from "@angular/core";
 import { HttpClient, HttpHeaders,HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { environment } from "src/environments/environment";
 import { Observable } from "rxjs";
+import { AuthenticationService } from "./AuthenticationService";
 import { JournalListingData } from "src/models/JournalListingData";
 
 @Injectable({
@@ -9,11 +10,23 @@ import { JournalListingData } from "src/models/JournalListingData";
 })
 export class JournalListingService {
    
-    constructor(private http: HttpClient) { }
-
+    constructor(private http: HttpClient,private authService : AuthenticationService) { }
+    /*
     public getBranchList(): Observable<any>{
       return this.http.get<any>(`${environment.baseUrl}`+'/jlisting-report/get_branchList');
+    }*/
+
+    //Access Branch >> parameter 1
+    public getBranchList(formatType: number): Observable<any>{
+      var userId=this.authService.currentUserValue.userId;
+      return this.http.get<any>(`${environment.baseUrl}`+'/latest-trial/get_branchList?userId='+userId + '&formatType=' + formatType);
     }
+	
+    //home branch 
+    public getBranchListForMigration(): Observable<any>{
+      return this.http.get<any>(`${environment.baseUrl}`+'/backDateTrial/migrationBranch');
+    }
+  
 
     public getCurrencyList(): Observable<any>{
       return this.http.get<any>(`${environment.baseUrl}`+'/jlisting-report/get_currencyList');
